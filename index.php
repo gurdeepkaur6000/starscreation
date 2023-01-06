9c7cbf8e28e1d8f7f958a90867e2e428988bdfe9
gurdeepkaur6000 <gurdeep.kaur6000@gmail.com>
2023-01-06 21:51:15 +0530

    updates

--- index.php
+++ index.php
@@ -35,3 +35,76 @@ for($i=1; $i<=5; $i++)
 	echo "<br />";
 }
 ?>
+
+=============================================================================================<br />
+
+
+*****<br />
+*****<br />
+*****<br />
+*****<br />
+*****<br />
+
+using code : <br />
+
+<?php 
+for($i=1; $i<=5; $i++)
+{
+	for($j=1; $j<=5; $j++)
+	{
+		echo "*";
+	}
+	echo "<br />";
+}
+?>
+
+=============================================================================================<br />
+
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**<br />
+&nbsp;&nbsp;&nbsp;&nbsp;***<br />
+&nbsp;&nbsp;****<br />
+*****<br />
+
+using code : <br />
+
+<?php 
+for($i=1; $i<=5; $i++)
+{
+	for($j=5; $j>=$i; $j--)
+	{
+		echo "&nbsp;&nbsp;";
+	}
+	for($k=1; $k<=$i; $k++)
+	{
+		echo "*";
+	}
+	echo "<br />";
+}
+?>
+
+=============================================================================================<br />
+
+*****<br />
+&nbsp;&nbsp;****<br />
+&nbsp;&nbsp;&nbsp;&nbsp;***<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;*<br />
+
+using code : <br />
+
+<?php 
+for($i=1; $i<=5; $i++)
+{
+	for($j=1; $j<=$i; $j++)
+	{
+		echo "&nbsp;&nbsp;";
+	}
+	for($k=5; $k>=$i; $k--)
+	{
+		echo "*";
+	}
+	echo "<br />";
+}
+?>
